03a16814333436e47e84ab6a54bfcaf029be0b9b
szhu5759 <szhu5759@gmail.com>
2019-05-22 02:51:31 +0000

    Automatically backed up by Learn

--- tt.js
+++ tt.js
@@ -2,4 +2,4 @@ const firstMeal = "breakfast"
 
 var meals = { [firstMeal]: "oatmeal" }
 
-console.log(meals.breakfast + "and " + meals['breakfast'] + " are the same")
+console.log(meals.breakfast + " and " + meals['breakfast'] + " are the same")
