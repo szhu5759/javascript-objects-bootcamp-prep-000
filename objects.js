4c2f3b8563089d9213f185927b0919942677a3b2
szhu5759 <szhu5759@gmail.com>
2019-05-22 02:31:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = new Object({ artist1: "song1" })
 
 function updatePlaylist(playlist,artistName,songTitle) {
-  var playlist = new Object({artistName: songTitle})
-  return playlist
+  return Object.assign({}, playlist, { [artistName]: songTitle})
 }
+
+s
