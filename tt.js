575ddf55c913d28878fb3ff537aed3996ac90eb2
szhu5759 <szhu5759@gmail.com>
2019-05-22 03:12:12 +0000

    Automatically backed up by Learn

--- tt.js
+++ tt.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 const music = {
   Rihanna: "Umbrella",
-  ['DeathCab for Cutie']: "I'll follow you into the dark" }
+  ['DeathCab for Cutie']: "I\'ll follow you into the dark" }
 
 updatePlaylist(music)
 
