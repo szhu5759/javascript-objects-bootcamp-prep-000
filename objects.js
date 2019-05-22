79b35168ce23fc1c3ab433d7f3e0293b75a8b773
szhu5759 <szhu5759@gmail.com>
2019-05-22 03:07:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,11 @@
-var playlist = { artist1 : "song1" }
+var playlist = { artistName : songTitle }
 
 //function called updatePlaylist
 //accepts three parameters (playlist, artistName, songTitle)
+//body of function add song and artist as key-value pair
+//returns whole playlist
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
