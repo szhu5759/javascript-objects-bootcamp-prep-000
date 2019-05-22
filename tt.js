4abd46e82acd14f98cfd577ac9ae9cf5952bb8bf
szhu5759 <szhu5759@gmail.com>
2019-05-22 03:32:51 +0000

    Automatically backed up by Learn

--- tt.js
+++ tt.js
@@ -1,12 +1,12 @@
 var kitten = { White: "Snow"}
 
 function assignObjects(kitten,name,color) {
-  
+
   return Object.assign({}, kitten, {[name]: color
   })
 }
 
-const meows = { 
+const meows = {
   White: "Snow",
   Black: "Midnight",
   Yellow "Honey",
