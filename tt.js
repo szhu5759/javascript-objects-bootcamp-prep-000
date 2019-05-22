4838ac228e07df67460edb5e2ea1bd7ef0fbd7c9
szhu5759 <szhu5759@gmail.com>
2019-05-22 02:49:25 +0000

    Automatically backed up by Learn

--- tt.js
+++ tt.js
@@ -1,7 +1,7 @@
 const firstMeal = "breakfast"
 //firstMeal set to "breakfast" with constant
 
-var meals = { firstMeal: "oatmeal" }
+var meals = { [firstMeal]: "oatmeal" }
 //firstMeal set to "oatmeal"
 
 //Let's see which one prints for us when calling meals
