9fedb0916927aa6dedb8af96f906e259e18a83fe
szhu5759 <szhu5759@gmail.com>
2019-05-22 02:51:24 +0000

    Automatically backed up by Learn

--- tt.js
+++ tt.js
@@ -1,8 +1,6 @@
 const firstMeal = "breakfast"
-//firstMeal set to "breakfast" with constant
 
 var meals = { [firstMeal]: "oatmeal" }
-//firstMeal set to "oatmeal"
 
-//Let's see which one prints for us when calling meals
-console.log(meals)
+console.log(meals.breakfast + "and " + meals['breakfast'] + " are the same")
+
