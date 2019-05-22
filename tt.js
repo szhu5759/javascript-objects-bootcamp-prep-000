0ff29e2aa52ee6ef082e766057235a418fded4e6
szhu5759 <szhu5759@gmail.com>
2019-05-22 02:52:36 +0000

    Automatically backed up by Learn

--- tt.js
+++ tt.js
@@ -4,3 +4,4 @@ var meals = { [firstMeal]: "oatmeal" }
 
 console.log(meals.breakfast + " and " + meals['breakfast'] + " are the same")
 console.log(meals[firstMeal])
+console.log("But when you use .notation like meals.firstMeal will result in " + meals.firstMeal)
