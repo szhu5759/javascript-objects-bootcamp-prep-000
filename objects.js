100790c576d1f33623cc178c050a0b2cedd4acba
szhu5759 <szhu5759@gmail.com>
2019-05-22 03:16:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,10 @@
 var playlist = { ['The Kooks'] : "Naive" }
 
-//function called updatePlaylist
-//accepts three parameters (playlist, artistName, songTitle)
-//body of function add song and artist as key-value pair
-//returns whole playlist
-
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
+}
+function removeFromPlaylist(artistName, songTitle) {
+  delete artist.songTitle
+  return playlist(artistName: songTitle)
 }
